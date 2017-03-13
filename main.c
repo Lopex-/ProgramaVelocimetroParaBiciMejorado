a154c42eda06e2e7211a577ed87d73d3e2b8321d
Lopex_ <ir.lopex@hotmail.com>
2017-03-13 04:10:43 -0600

    Prueba 2 - Botones
    Se añadieron las definiciones para los botones ademas de una correción
    en las definiciones de apagado para los leds

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 /*
  * main.c
  *
- * Prueba 01 del programa para la placa Vlocimetro para bicicleta mejorado
+ * Prueba 1 del programa para la placa Velocimetro para bicicleta mejorado
  * Primer test del ICSP de la placa
  * Simpe parpadeo de leds
+ *
+ * Prueba 2. Se añadieron las definiciones para los botones
+ * correcion en las definiciones para apagar los leds
  *
  * Created: 12/03/2017 15:02:42 
  *  Author: Lopex_
@@ -18,6 +21,8 @@
 
 // ------------------------------- Dfiniciones ------------------------------------
 
+// LEDS
+
  #define mask_LED1	0
  #define mask_LED2	1
 
@@ -30,14 +35,47 @@
  #define init_LED1	conf_LED1 |= 1<<mask_LED1
  #define init_LED2	conf_LED2 |= 1<<mask_LED2
 
- #define LED1_ON	port_LED1 &= 0<<mask_LED1
- #define LED2_ON	port_LED2 &= 0<<mask_LED2
+ #define LED1_OFF	port_LED1 &= ~(1<<mask_LED1)
+ #define LED2_OFF	port_LED2 &= ~(1<<mask_LED2)
 
- #define LED1_OFF	port_LED1 |= 1<<mask_LED1
- #define LED2_OFF	port_LED2 |= 1<<mask_LED2
+ #define LED1_ON	port_LED1 |= 1<<mask_LED1
+ #define LED2_ON	port_LED2 |= 1<<mask_LED2
 
  #define LED1_SWAP	port_LED1 ^= 1<<mask_LED1
  #define LED2_SWAP	port_LED2 ^= 1<<mask_LED2
+
+
+// BOTONES
+
+ #define mask_BTN1          2
+ #define mask_BTN2          3
+ #define mask_BTN3          4
+
+ #define conf_BTN1          DDRC
+ #define conf_BTN2          DDRC
+ #define conf_BTN3          DDRC
+
+ #define port_BTN1          PINC
+ #define port_BTN2          PINC
+ #define port_BTN3          PINC
+
+ #define pullup_BTN1        PORTC
+ #define pullup_BTN2        PORTC
+ #define pullup_BTN3        PORTC    
+
+ #define init_BTN1          conf_BTN1 &= 0<<mask_BTN1
+ #define init_BTN2          conf_BTN2 &= 0<<mask_BTN2
+ #define init_BTN3          conf_BTN3 &= 0<<mask_BTN3
+
+ #define enable_pullup_BTN1 pullup_BTN1 |= 1<<mask_BTN1
+ #define enable_pullup_BTN2 pullup_BTN2 |= 1<<mask_BTN2
+ #define enable_pullup_BTN3 pullup_BTN3 |= 1<<mask_BTN3
+
+ #define BTN1_PRESS         ~(port_BTN1>>mask_BTN1)&1
+ #define BTN2_PRESS         ~(port_BTN2>>mask_BTN2)&1
+ #define BTN3_PRESS         ~(port_BTN3>>mask_BTN3)&1
+
+
 
 // ---------------------------- Programa principal --------------------------------
 
@@ -46,13 +84,34 @@ int main(void)
 	init_LED1;						//Inicializa los puertos como salidas
 	init_LED2;
 
+    init_BTN1;                      //Configura los pines como entradas
+    init_BTN2;
+    init_BTN3;
+
+    enable_pullup_BTN1;             //Habilita las resistencias internas de los pines
+    enable_pullup_BTN2;
+    enable_pullup_BTN3;
+
 	LED1_OFF;						//Los leds comenzaran la secencia uno encendido y uno apagado
 	LED2_OFF;
 
-    while(1)
-    {
-        LED1_SWAP;					//Los leds cambiaran de estado infinitmente cada medio segundo
-        _delay_ms(500);
-        LED2_SWAP;
+    while(1){                       //Dependiendo el boton presionado se iluminaran los leds
+        if(BTN1_PRESS){
+            LED1_ON;
+            LED2_OFF;
+        }
+        else if(BTN2_PRESS){
+            LED1_OFF;
+            LED2_ON;
+        }
+        else if(BTN3_PRESS){
+            LED1_ON;
+            LED2_ON;
+        }
+        else{
+            LED1_OFF;
+            LED2_OFF;
+        }
+        _delay_ms(100);
     }
 }
